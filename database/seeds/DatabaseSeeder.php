7b37058dbd3daa47f3b40f3e48c56a75c48e880b
Michel <michel@flashpoint.nl>
2017-09-12 13:16:08 +0200

    Added DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('product_information')->insert([
+            'product_name' => str_random(10),
+            'sku' => str_random(2) . random_int(1000, 9999) . str_random(2) . random_int(10, 99),
+            'ean' => random_int(0, 99),
+            'price' => random_int(0, 999) / 100,
+            'brand' => str_random(20),
+            'size' => random_int(0, 10) * 100,
+            'stock' => random_int(0, 1000)
+        ], 50);
     }
 }
